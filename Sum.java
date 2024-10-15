f941d684710b21755268f982aa612a3523f30334
christian <christianmary@gmail.com>
2024-10-15 05:14:26 +0100

    first commit

---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum{
+ public static void main (String[] args){
+  int number1 = 2;
+  int number2 = 4;
+  int number3 = 10;
+  int sum = (number2*number3) + (number1*number2/2);
+  System.out.print("sum is 44"); 
+  System.out.println("\nsum is 44");
+  System.out.printf("sum is %d", sum);
+ }
+} 
