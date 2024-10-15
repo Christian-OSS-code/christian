8990083012eecdf05d206e04cf4eb7699e1bf70a
christian <christianmary@gmail.com>
2024-10-15 05:42:56 +0100

    first commit

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello{
+ public static void main(String[] arg){
+  System.out.print("Hello World");
+  System.out.println("\nHello World");
+  System.out.printf("%s%n", "Hello World");
+ }
+}
