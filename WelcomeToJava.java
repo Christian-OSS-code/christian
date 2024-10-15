9a6466b73a0161236c9c12dff6f704215dc404a7
christian <christianmary@gmail.com>
2024-10-15 05:32:33 +0100

    first commit

---
+++ WelcomeToJava.java
@@ -0,0 +1,7 @@
+public class WelcomeToJava{
+ public static void main(String[] args){
+  System.out.print("Welccome to java 101");
+  System.out.println("\nWelcome to java 101");
+  System.out.printf("%s%d" "Welcome to java", "101");
+ }
+}
