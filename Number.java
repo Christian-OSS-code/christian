d2d1e096f0b321fd9e4e9b22b21f04a61547a0c8
christian <christianmary@gmail.com>
2024-10-15 05:36:40 +0100

    first commit

---
+++ Number.java
@@ -0,0 +1,12 @@
+public class Number{
+ public static void main(String[] args){
+  int number1 = 2;
+  int number2 = 3;
+  int number3 = 4;
+  int number4 = 7;
+  int number5 = 9;
+  int number6 = 10;
+  int number = (number2 + number3 + number4 + number5 + number6)/(number1);
+  System.out.printf("number is %d", number);
+ }
+}
