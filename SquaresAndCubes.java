d207f3fc45b1b6bfbff04738c21385e272aafed4
christian <christianmary@gmail.com>
2024-10-15 05:25:22 +0100

    first commit

---
+++ SquaresAndCubes.java
@@ -0,0 +1,50 @@
+public class SquaresAndCubes{
+ public static void main(String[] args){
+  int integer1 = 0;
+  int total = 0;
+  int total0 = (integer1) + total;
+  int square0 = (total0)*(total0);
+  int total1 = (total0) + 1;
+  int square1 = (total1)*(total1);
+  int total2 = (total1) + 1;
+  int square2 = (total2)*(total2);
+  int total3 = (total2) + 1;
+  int square3 = (total3)*(total3);
+  int total4 = (total3) + 1;
+  int square4 = (total4)*(total4);
+  int total5 = (total4) + 1;
+  int square5 = (total5)*(total5);
+  int total6 = (total5) + 1;
+  int square6 = (total6)*(total6);
+  int total7 = (total6) + 1;
+  int square7 = (total6)*(total6);
+  int total8 = (total7) + 1;
+  int square8 = (total8)*(total8);
+  int total9 = (total8) + 1;
+  int square9 = (total9)*(total9);
+  int total10 = (total9) + 1;
+  int square10 = (total10)*(total10);
+  int total11 = (total10) + 1;
+  int square11 = (total10)*(total10);
+  
+  int cube0 = (total0)*(total0)*(total0);
+  int cube1 = (total1)*(total1)*(total1);
+  int cube2 = (total2)*(total2)*(total2);
+  int cube3 = (total3)*(total3)*(total3);
+  int cube4 = (total4)*(total4)*(total4);
+  int cube5 = (total5)*(total5)*(total5);
+  int cube6 = (total6)*(total6)*(total6);
+  int cube7 = (total7)*(total7)*(total7);
+  int cube8 = (total8)*(total8)*(total8);
+  int cube9 = (total9)*(total9)*(total9);
+  int cube10 = (total10)*(total10)*(total10);
+
+  System.out.printf("%d   %d   %d%n%d   %d   %d%n%d   %d   %d%n%d   %d   %d%n%d   %d  %d%n%d   %d  %d%n%d   %d  %d%n%d   %d  %d%n%d   %d  %d%n%d   %d  %d%n%d  %d %d", total0, square0, cube0, total1, square1, cube1, total2, square2, cube2, total3, square3, cube3, total4, square4, cube4, total5, square5, cube5, total6, square6, cube6, total7, square7, cube7, total8, square8, cube8, total9, square9, cube9, total10, square10, cube10);
+ }
+}
+  
+  
+
+
+  
+   
